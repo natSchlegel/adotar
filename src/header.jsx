52393d9f81f5368ef773218730d28e4bf2c99621
Natascha <46815651+natSchlegel@users.noreply.github.com>
2024-11-28 21:30:05 +0100

    Classname

--- src/header.jsx
+++ src/header.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import styles from "./header.module.css";
 
 const header = () => {
   return (
     <div>
-      <header class="p-3 text-bg-dark">
-        <div class="container">
-          <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+      <header className="p-3 text-bg-dark">
+        <div className="container">
+          <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
             <a
               href="/"
-              class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
+              className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
             >
               <svg
-                class="bi me-2"
+                className="bi me-2"
                 width="40"
                 height="32"
                 role="img"
@@ -21,34 +22,34 @@ const header = () => {
               </svg>
             </a>
 
-            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+            <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
               <li>
-                <a href="#" class="nav-link px-2 text-secondary">
+                <a href="#" className="nav-link px-2 text-secondary">
                   Início
                 </a>
               </li>
               <li>
-                <a href="#" class="nav-link px-2 text-white">
+                <a href="#" className="nav-link px-2 text-white">
                   Sobre Nós
                 </a>
               </li>
               <li>
-                <a href="#" class="nav-link px-2 text-white">
+                <a href="#" className="nav-link px-2 text-white">
                   Doações
                 </a>
               </li>
               <li>
-                <a href="#" class="nav-link px-2 text-white">
+                <a href="#" className="nav-link px-2 text-white">
                   Perguntas Frequentes
                 </a>
               </li>
             </ul>
 
-            <div class="text-end">
-              <button type="button" class="btn btn-outline-light me-2">
+            <div className="text-end">
+              <button type="button" className="btn btn-outline-light me-2">
                 Entrar
               </button>
-              <button type="button" class="btn btn-warning">
+              <button type="button" className="btn btn-warning">
                 Registrar
               </button>
             </div>
--- src/search.jsx
+++ src/search.jsx
@@ -4,84 +4,14 @@ import Female from "../img/female.svg?react";
 import Male from "../img/male.svg?react";
 import Location from "../img/location.svg?react";
 import Cat from "../img/cat-3297664_1280.jpg";
-import styles from "./search.module.css";
+import SearchRect from "./searchRect";
 
 const search = () => {
   return (
-    <div class="album py-5 bg-body-tertiary">
-      <div class="container">
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          <div class="col">
-            {/* <div class="card shadow-sm">
-              <img
-                className={`bd-placeholder-img card-img-top ${styles.cat}`}
-                width="100%"
-                src={Cat}
-              />
-              <div class="card-body">
-                <div style="height: 40px; display: flex; justify-content: space-between;">
-                  <h4>Nome do Animal</h4>
-                  <div>
-                    <Favorite />
-                    <Female />
-                  </div>
-                </div>
-                <div>
-                  <Location /> Diamantina - MG
-                </div>
-                <div style="display: flex; gap: 20px;">
-                  <div
-                    style="width: 100px;
-                  height: 100px;
-                  display: flex;
-                  flex-direction: column;
-                  justify-content: center;
-                  align-items: center;
-                  background-color: #fce4ec;  
-                  border-radius: 15px;
-                  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-                  text-align: center;"
-                  >
-                    Peso
-                  </div>
-                  <div
-                    style="width: 100px;
-                  height: 100px;
-                  display: flex;
-                  flex-direction: column;
-                  justify-content: center;
-                  align-items: center;
-                  background-color: #fce4ec; 
-                  border-radius: 15px;
-                  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-                  text-align: center;"
-                  >
-                    Idade
-                  </div>
-                  <div
-                    style="width: 100px;
-                  height: 100px;
-                  display: flex;
-                  flex-direction: column;
-                  justify-content: center;
-                  align-items: center;
-                  background-color: #fce4ec; 
-                  border-radius: 15px;
-                  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-                  text-align: center;"
-                  >
-                    Porte
-                  </div>
-                </div>
-
-                <p class="card-text" style="padding-top: 10px;">
-                  This is a wider card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </p>
-              </div>
-            </div> */}
-          </div>
+    <div className="album py-5 bg-body-tertiary">
+      <div className="container">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+          <SearchRect /><SearchRect /><SearchRect /><SearchRect /><SearchRect /><SearchRect /><SearchRect /><SearchRect />
         </div>
       </div>
     </div>
