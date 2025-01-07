4405200707cc34ad9545689f3e24812b5ab8ef56
Natascha <46815651+natSchlegel@users.noreply.github.com>
2025-01-08 00:24:01 +0100

    deploy

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./Home";
 import Sobre from "./Sobre";
 import Header from  "./Home/Header/Header";
@@ -7,16 +7,16 @@ import Adoption from "./Home/Adoption/Adoption";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
+    <>
+      <HashRouter>
       <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="sobre/*" element={<Sobre />} />
           <Route path="adocao/" element={<Adoption />} />
         </Routes>
-      </BrowserRouter>
-    </div>
+      </HashRouter>
+    </>
   );
 }
 
