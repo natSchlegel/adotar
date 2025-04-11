72dbcc9f24d1dc6b303045f17be5f8c40a45de88
Natascha <46815651+natSchlegel@users.noreply.github.com>
2025-04-11 16:36:30 +0200

    new commit

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,18 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Logo from "../img/logoinline.svg";
 
 const Footer = () => {
   return (
-    <div>
-      <br/>
-
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-    </div>
+    <div style={{ backgroundColor: "#604543", display: "flex", justifyContent: "center" }}>
+<img src={Logo} style={{ padding: "40px"}} />
+</div>
   )
 }
 
